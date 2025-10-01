68b47d6b5ae3ca703d0cc28fc746cd8d624654c0
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-10-01 23:55:31 +0500

    27 update ok

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import CustomLogoutView, RegisterView
+from users.views import CustomLogoutView, RegisterView, UsersListView
 
 app_name = UsersConfig.name
 
@@ -9,6 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/', CustomLogoutView.as_view(),name='logout'),
     path('register/', RegisterView.as_view(),name='register'),
-
-
+    path("list/", UsersListView.as_view(), name="users_list"),
 ]
